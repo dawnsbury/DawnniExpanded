2b58540ea6f7a7c6098a4bd66310e2a04f9a5a0e
Dawnsbury Studios <dawnsbury@gmail.com>
2024-05-01 07:05:05 +0200

    align mods

--- Misc/Skills.cs
+++ Misc/Skills.cs
@@ -10,9 +10,9 @@ namespace Dawnsbury.Mods.DawnniExpanded
         public static Feat Performance = new SkillSelectionFeat(FeatName.CustomFeat, Skill.Performance, Trait.Performance).WithCustomName("Performance");
         public static Feat Crafting = new SkillSelectionFeat(FeatName.CustomFeat, Skill.Crafting, Trait.Crafting).WithCustomName("Crafting");
         public static Feat Survival = new SkillSelectionFeat(FeatName.CustomFeat, Skill.Survival, Trait.Survival).WithCustomName("Survival");
-        public static Feat ExpertPerformance = new SkillIncreaseFeat(FeatName.CustomFeat, Skill.Performance, Trait.Performance).WithCustomName("Expert in Performance");
-        public static Feat ExpertCrafting = new SkillIncreaseFeat(FeatName.CustomFeat, Skill.Crafting, Trait.Crafting).WithCustomName("Expert in Crafting");
-        public static Feat ExpertSurvival = new SkillIncreaseFeat(FeatName.CustomFeat, Skill.Survival, Trait.Survival).WithCustomName("Expert in Survival");
+        public static Feat ExpertPerformance = new SkillIncreaseFeat(FeatName.CustomFeat, Skill.Performance, Trait.Performance, Proficiency.Expert).WithCustomName("Expert in Performance");
+        public static Feat ExpertCrafting = new SkillIncreaseFeat(FeatName.CustomFeat, Skill.Crafting, Trait.Crafting, Proficiency.Expert).WithCustomName("Expert in Crafting");
+        public static Feat ExpertSurvival = new SkillIncreaseFeat(FeatName.CustomFeat, Skill.Survival, Trait.Survival, Proficiency.Expert).WithCustomName("Expert in Survival");
 
 
         public static void LoadMod()
